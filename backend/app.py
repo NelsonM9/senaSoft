62e4b68d397d879fd9712d1a49c7658592a2b363
DarkMatter <juxxon23@hotmail.com>
2020-10-15 12:52:41 -0500

    Se agrega autorizaciones y ordenes

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
-from flask import  Flask
+from flask import Flask
 from flask_cors import CORS
-from routes import users
+from routes import users, document
 from db.postgresql.model import db
 
 app = Flask(__name__)
 
-CORS(app, support_credentials = True)
+CORS(app, support_credentials=True)
 db.init_app(app)
 
 # User routes
 app.add_url_rule(users['signin'], view_func=users['view_func_signin'])
 app.add_url_rule(users['login'], view_func=users['view_func_login'])
 
-# Appointments route
+# Documents route
+app.add_url_rule(document['authorization'],
+                 view_func=document['view_func_authorization'])
+app.add_url_rule(document['order'], view_func=document['view_func_order'])
--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,15 @@
 from controllers.signin import Signin
 from controllers.login import Login
 from controllers.appointment import Appointment
-from controllers.order import  Order
+from controllers.order import Order
 from controllers.authorization import Authorization
 
 users = {
     "signin": "/signin", "view_func_signin": Signin.as_view("api_signin"),
     "login": "/login", "view_func_login": Login.as_view("api_login")
+}
+
+document = {
+    "authorization": "/authorization/<id_p>", "view_func_authorization": Authorization.as_view("api_authorization"),
+    "order": "/order/<id_p>", "view_func_order": Order.as_view("api_order"),
 }
