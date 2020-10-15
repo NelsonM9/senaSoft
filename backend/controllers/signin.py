b9c97c87902ee5c262e1c7c8c2bb4b34719c04be
DarkMatter <juxxon23@hotmail.com>
2020-10-15 15:17:04 -0500

    Se ajustaron validadores del signin

--- backend/controllers/signin.py
+++ backend/controllers/signin.py
@@ -22,7 +22,10 @@ class Signin(MethodView):
             if my_db == "error":
                 raise Exception
             try:
-                if user_signin['role_d'] == "1":
+                if user_signin['role_d'] == '1':
+                    errors = doctor_schema.validate(user_signin)
+                    if errors:
+                        return jsonify({'st': errors})
                     docs = cm.get_query_by(
                         my_db, user_signin['mail_d'], 'mail_d')
                     if docs == []:
@@ -34,22 +37,25 @@ class Signin(MethodView):
                         elif doc_msg == "error":
                             return jsonify({'st': 'add error'}), 403
                     else:
-                        return jsonify({'st': 'exists'})
+                        return jsonify({'st': 'exists'}),  403
             except:
                 try:
-                    if user_signin['role_p'] == "2":
+                    if user_signin['role_p'] == '2':
+                        errors = patient_schema.validate(user_signin)
+                        if errors:
+                            return jsonify({'st': errors})
                         docs = cm.get_query_by(
                             my_db, user_signin['mail_p'], 'mail_p')
-                    if docs == []:
-                        user_signin['password_p'] = crypt.hash_string(
-                            user_signin['password_p'])
-                        doc_msg = cm.add_doc(my_db, user_signin)
-                        if doc_msg == "ok":
-                            return jsonify({'st': 'ok'}), 200
-                        elif doc_msg == "error":
-                            return jsonify({'st': 'add error'}), 403
-                    else:
-                        return jsonify({'st': 'exists'})
+                        if docs == []:
+                            user_signin['password_p'] = crypt.hash_string(
+                                user_signin['password_p'])
+                            doc_msg = cm.add_doc(my_db, user_signin)
+                            if doc_msg == "ok":
+                                return jsonify({'st': 'ok'}), 200
+                            elif doc_msg == "error":
+                                return jsonify({'st': 'add error'}), 403
+                        else:
+                            return jsonify({'st': 'exists'}), 403
                 except:
                     return jsonify({'st': 'incorrect data'}), 403
         except:
